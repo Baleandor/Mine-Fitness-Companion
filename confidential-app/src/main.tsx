43fe861ba41ac7eac18cb7b394b5d9587ea3d616
Baleandor <overpoweredmedia@gmail.com>
2023-06-14 17:31:51 +0300

    Login functionality & Navbar
    Implemented bare-bones Navbar functionality.
    Implemented Login functionality along with some integration with the faux backend and login input error handling. Some styles were also applied.

--- confidential-app/src/main.tsx
+++ confidential-app/src/main.tsx
@@ -2,15 +2,22 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Home from './pages/home';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Root from './pages/Root';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Root />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ]
   },
 ]);
 
---
+++ confidential-app/src/mockBackend/users.ts
@@ -0,0 +1,13 @@
+export const userOne = new Map<string, string>()
+
+userOne.set('id', '1')
+userOne.set('name', 'Jotaro')
+userOne.set('email', 'jotaro@oraora.com')
+userOne.set('password', 'jolyne')
+userOne.set('role', 'user')
+userOne.set('gender', 'male')
+userOne.set('dateOfBirth', '1')
+userOne.set('height', '195')
+
+
+

b21595254c8bdd800a1f4a55adfa57c3496d6a83
Baleandor <overpoweredmedia@gmail.com>
2023-06-15 19:59:19 +0300

    Register page implemented

--- confidential-app/src/pages/Navbar.tsx
+++ confidential-app/src/pages/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="flex">
             <Link to={'/login'} className="p-2">Login</Link>
-            <Link to={'/'} className="p-2">Register</Link>
+            <Link to={'/register'} className="p-2">Register</Link>
             <Link to={'/'} className="p-2">Profile</Link>
             <Link to={'/'} className="p-2">Logout</Link>
         </nav>

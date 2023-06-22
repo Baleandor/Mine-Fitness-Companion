7fcf63059d8e0f8c781dc2d9025e690b58ba2d83
Baleandor <overpoweredmedia@gmail.com>
2023-06-22 16:23:33 +0300

    Navbar Logged User Polish

--- confidential-app/src/mockBackend/users.ts
+++ confidential-app/src/mockBackend/users.ts
@@ -9,5 +9,16 @@ userOne.set('gender', 'male')
 userOne.set('dateOfBirth', '01-01-1986')
 userOne.set('height', '195')
 
+export const userTwo = new Map<string, string>()
+
+userTwo.set('id', '2')
+userTwo.set('name', 'Dio')
+userTwo.set('email', 'dio@mudamuda.com')
+userTwo.set('password', 'wryyyy')
+userTwo.set('role', 'admin')
+userTwo.set('gender', 'male')
+userTwo.set('dateOfBirth', '01-01-1986')
+userTwo.set('height', '195')
+
 
 
--- confidential-app/src/pages/Navbar.tsx
+++ confidential-app/src/pages/Navbar.tsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
 
+
 export default function Navbar() {
+
+    const user = localStorage.getItem('user')
+
 
     return (
         <nav className="flex">
-            <Link to={'/login'} className="p-2">Login</Link>
-            <Link to={'/register'} className="p-2">Register</Link>
-            <Link to={'/user-profile'} className="p-2">Profile</Link>
-            <Link to={'/'} className="p-2">Logout</Link>
+            {user ?
+                <div>
+                    <Link to={'/user-profile'} className="p-2">Profile</Link>
+                    <Link to={'/'} className="p-2">Logout</Link>
+
+                </div> :
+                <div>
+                    <Link to={'/login'} className="p-2">Login</Link>
+                    <Link to={'/register'} className="p-2">Register</Link>
+                </div>
+            }
+
         </nav>
     )
 }

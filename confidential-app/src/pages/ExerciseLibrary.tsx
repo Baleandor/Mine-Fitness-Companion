3c4c9d53c3a6f18b0e1073adf93c28b856cad080
Baleandor <overpoweredmedia@gmail.com>
2023-06-23 15:56:32 +0300

    Exercise library search

---
+++ confidential-app/src/pages/ExerciseLibrary.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import { exerciseTypes } from "../mockBackend/exerciseType"
+
+
+
+
+export default function ExerciseLibrary() {
+
+    const [searchParams, setSearchParams] = useState('')
+
+    const [searchResult, setSearchResult] = useState([])
+
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchParams(event.target.value.toLocaleLowerCase())
+    }
+
+    const search = () => {
+        const search = []
+        for (let key in exerciseTypes) {
+            if (searchParams.includes(exerciseTypes[key].name.toLocaleLowerCase())) {
+                search.push(exerciseTypes[key])
+            }
+            if (exerciseTypes[key].muscleGroups.includes(searchParams)) {
+                search.push(exerciseTypes[key])
+            }
+        }
+        setSearchResult(search)
+    }
+
+    return (
+        <div className="flex flex-col items-center p-2">
+            <div>
+                <span className="p-1">Search Exercise</span>
+                <input type="search" onChange={handleOnChange} className="p-1" ></input>
+                <button onClick={search} className="p-1 border border-red-400 rounded-md bg-">Find</button>
+            </div>
+            <div className="p-1">
+                {searchResult.length > 0 && searchResult.map((result) => {
+                    return (
+                        <div key={result.id}>
+                            <span >{result.name}</span>
+                            <span className="ml-2">{result.muscleGroups.join(" ")}</span>
+                        </div>
+                    )
+                })}
+
+            </div>
+
+        </div>
+    )
+}

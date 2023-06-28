9c400bf6ad163edc44a000e61af98871725b9ea9
Tsvetan <overpoweredmedia@gmail.com>
2023-06-28 12:12:54 +0300

    User Workouts Search (#17)

--- confidential-app/src/mockBackend/exerciseType.ts
+++ confidential-app/src/mockBackend/exerciseType.ts
@@ -1,17 +1,17 @@
 export const exerciseTypes = {
     deadlift: {
         id: 1,
-        name: 'Deadlift',
+        name: 'deadlift',
         muscleGroups: ['back', 'legs', 'posterior chain']
     },
     squat: {
         id: 2,
-        name: 'Squat',
+        name: 'squat',
         muscleGroups: ['legs', 'hips']
     },
     benchPress: {
         id: 3,
-        name: 'Bench Press',
+        name: 'bench press',
         muscleGroups: ['chest', 'shoulders', 'triceps']
     }
 }

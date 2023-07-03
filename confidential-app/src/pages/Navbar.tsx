f307b121ffc06ddcc50219b3623e82b7b3f92d58
Baleandor <overpoweredmedia@gmail.com>
2023-07-03 18:03:15 +0300

    User Profile Revamped

--- confidential-app/src/pages/Navbar.tsx
+++ confidential-app/src/pages/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { logout } from "../redux/authSlice";
 import { ROUTE_PATH } from "../util/urls";
+import { userPermittedActions } from "../api/userPermittedActions";
 
 
 
@@ -18,24 +19,28 @@ export default function Navbar() {
         navigate(ROUTE_PATH.HOME)
     }
 
+    const buttonTest = () => {
+        userPermittedActions.getUserBasicInfo()
+    }
 
     return (
         <nav className="flex">
-            <Link to={ROUTE_PATH.HOME} className="p-2" />
+            <Link to={ROUTE_PATH.HOME} className="p-2">Home</Link>
+
             {user != null ?
-                <div>
+                <>
                     <Link to={ROUTE_PATH.USER_PROFILE} className="p-2">Profile</Link>
                     <button className="p-2" onClick={onLogout}>Logout</button>
 
-                </div> :
-                <div>
+                </> :
+                <>
                     <Link to={ROUTE_PATH.LOGIN} className="p-2">Login</Link>
                     <Link to={ROUTE_PATH.REGISTER} className="p-2">Register</Link>
-                </div>
+                </>
             }
-            <div>
-                <Link to={ROUTE_PATH.EXERCISE_LIBRARY} className="p-2">Exercise Library</Link>
-            </div>
+
+            <Link to={ROUTE_PATH.EXERCISE_LIBRARY} className="p-2">Exercise Library</Link>
+            <button onClick={buttonTest}>Test</button>
         </nav>
     )
 }

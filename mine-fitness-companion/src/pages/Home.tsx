f7f31f06843be64e957cf7406f138c068eb23fa8
Baleandor <overpoweredmedia@gmail.com>
2025-07-15 15:43:03 +0300

    Update to Home

--- mine-fitness-companion/src/pages/Home.tsx
+++ mine-fitness-companion/src/pages/Home.tsx
@@ -4,6 +4,6 @@ export default function Home() {
 
 
     return (
-        <div className="bg-green-500 p-5">Home base</div>
+        <div className="bg-green-500 p-5">Welcome to the Fitness Companion 9420</div>
     )
 }
